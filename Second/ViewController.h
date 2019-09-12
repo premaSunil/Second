59046686a00483aba8a0265509a66e2a43f12170
premaSunil <sunil.godumala>
2019-09-12 11:38:12 +0530

    Initial Commit

---
+++ Second/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Second
+//
+//  Created by sunil on 12/09/19.
+//  Copyright © 2019 sunil. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Second/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  Second
+//
+//  Created by sunil on 12/09/19.
+//  Copyright © 2019 sunil. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
